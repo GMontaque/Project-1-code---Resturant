3a52973dc87f9a3baf4003758ccc1f1e4ece5269
Giulio <62805702+knightrider909@users.noreply.github.com>
2021-05-28 20:22:22 +0100

    changes

--- javascript.js
+++ javascript.js
@@ -1,9 +1,49 @@
+// ******************************* making reservation *******************************
+
+let objects = {};
+let form = document.getElementById("form");
+form.onsubmit = function (e) {
+	let name = document.getElementById("name").value;
+	let email = document.getElementById("email").value;
+	let partySize = document.getElementById("partySize").value;
+	let date = document.getElementById("date").value;
+	let time = document.getElementById("time").value;
+	let location = document.getElementById("location").value;
+	let contactNumber = document.getElementById("contactNumber").value;
+	let text = document.getElementById("my-textarea").value;
+
+	let result = Math.random().toString().slice(2, 11);
+	objects = {
+		result: {
+			name: name,
+			email: email,
+			partySize: partySize,
+			date: date,
+			time: time,
+			location: location,
+			contactNumber: contactNumber,
+			text: text,
+			reservation: result,
+		},
+	};
+	console.log(JSON.stringify(objects));
+	e.preventDefault();
+};
+
+// party size feild
 $(function () {
-	var $select = $(".partySize");
+	var $select = $("#partySize");
 	for (i = 0; i <= 20; i++) {
 		$select.append($("<option></option>").val(i).html(i));
 	}
 });
 
-let tester = document.querySelector(".partySize");
-console.log(tester);
+// ******************************* checking reservation *******************************
+let refValue = document.querySelector(".todo-input");
+let modal = document.querySelector(".todo-button");
+modal.addEventListener("click", popUp);
+
+function popUp() {
+	Swal.fire(`${refValue.value}`);
+	refValue.value = "";
+}
