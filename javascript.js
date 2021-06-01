4d0b28d5e2ae33aabb1a357f9e44f3bdb1a7c73b
Giulio <62805702+knightrider909@users.noreply.github.com>
2021-06-01 20:55:27 +0100

    update localstorage

--- javascript.js
+++ javascript.js
@@ -1,38 +1,45 @@
 // ******************************* making reservation *******************************
 
 let submitBtn = document.querySelector("#resSubmit");
-submitBtn.addEventListener("click", confirmRev);
+submitBtn.addEventListener("click", reservationResult);
 
 // look to have all inputs in one function and have them passed to the modal popup
 
-function confirmRev() {
-	let name = document.getElementById("name").value;
-	let email = document.getElementById("email").value;
-	let party = document.getElementById("partySize").value;
-	let date = document.getElementById("date").value;
-	let time = document.getElementById("time").value;
-	let location = document.getElementById("location").value;
-	// let contactNumber = document.getElementById("contactNumber").value;
-	let text = document.getElementById("my-textarea").value;
-	let reservation = Math.floor(Math.random() * 1000000000);
+function reservationResult() {
+	let glass = {
+		name: document.getElementById("name").value,
+		email: document.getElementById("email").value,
+		party: document.getElementById("partySize").value,
+		date: document.getElementById("date").value,
+		time: document.getElementById("time").value,
+		location: document.getElementById("location").value,
+		// let contactNumber = document.getElementById("contactNumber").value;
+		text: document.getElementById("my-textarea").value,
+		reservation: Math.floor(Math.random() * 1000000000),
+	};
 
+	confirmRev(glass);
+	saveLocalTodos(glass);
+}
+
+function confirmRev(glass) {
 	Swal.fire({
 		icon: "success",
 		title: `We are pleased to confim your reservation has been submitted.
-		<br/>Reservation Number: ${reservation}`,
+		<br/>Reservation Number: ${glass.reservation}`,
 		html: `Summary Reservation Information:
 		<br/>
-		Booking Name: ${name} 
+		Booking Name: ${glass.name} 
 		<br/>
-		Group Size: ${party} 
+		Group Size: ${glass.party} 
 		<br/>
-		Confirmation Email Address: ${email}
+		Confirmation Email Address: ${glass.email}
 		<br/>
-		Booking date and time: ${time} on ${date}
+		Booking date and time: ${glass.time} on ${glass.date}
 		<br/>
-		Location: ${location}
+		Location: ${glass.location}
 		<br/>
-		Notes: ${text}`,
+		Notes: ${glass.text}`,
 
 		showCancelButton: true,
 		confirmButtonText: "Close",
@@ -44,106 +51,27 @@ function confirmRev() {
 		cancelButtonColor: "red",
 	}).then((result) => {
 		if (!result.isConfirmed) {
-			// send user to alteration page
+			let removed = document.getElementById("collapseOne");
+			removed.classList.remove("show");
+
+			let added = document.getElementById("collapseTwo");
+			added.classList.add("show");
 		}
 	});
 
-	name = document.getElementById("name").value = "";
+	setTimeout(reset, 10000);
+}
+
+function reset() {
+	names = document.getElementById("name").value = "";
 	email = document.getElementById("email").value = "";
 	party = document.getElementById("partySize").value = "";
 	date = document.getElementById("date").value = "";
 	time = document.getElementById("time").value = "";
 	location = document.getElementById("location").value = "";
-	// let contactNumber = document.getElementById("contactNumber").value= "";
+	// glass.contactNumber = document.getElementById("contactNumber").value= "";
 	text = document.getElementById("my-textarea").value = "";
 }
-
-// let result = Math.random().toString().slice(2, 11);
-// 	let name = `${refValue.value}`;
-// 	let email = "test@test.com";
-// 	let party = 5;
-// 	let date = "23/07/2021";
-// 	let time = "13:20";
-// 	let location = "london";
-// 	let phoneNumber = "01733 3585417";
-// 	let textBox =
-// 		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, alias?";
-// 	let reservation = Math.floor(Math.random() * (1000 - 100) + 100);
-
-// let objects = {};
-// let name = document.getElementById("name").value;
-// let email = document.getElementById("email").value;
-// let partySize = document.getElementById("partySize").value;
-// let date = document.getElementById("date").value;
-// let time = document.getElementById("time").value;
-// let location = document.getElementById("location").value;
-// let contactNumber = document.getElementById("contactNumber").value;
-// let text = document.getElementById("my-textarea").value;
-// let result = Math.random().toString().slice(2, 11);
-
-// objects = {
-// 	result: {
-// 		name: name,
-// 		email: email,
-// 		partySize: partySize,
-// 		date: date,
-// 		time: time,
-// 		location: location,
-// 		contactNumber: contactNumber,
-// 		text: text,
-// 		reservation: result,
-// 	},
-// };
-
-// function confirmationPopup() {
-// 	Swal.fire({
-// 		icon: "success",
-// 		title: `Reservation Number: `,
-// 		html: `Booking Name: ${name} <br> Group Size: ${partySize} <br>
-//   Booking Date: ${date} <br> <input type="text" name="" id="inputText" placeholder="teser">`,
-// 		showCancelButton: true,
-// 		confirmButtonText: "Close",
-// 		cancelButtonText: `<a
-// 		style="color: white;"
-// 		href="#collapseOne"
-// 		>Make a Change</a
-// 	>`,
-// 		cancelButtonColor: "red",
-// 		// backdrop: "lightgrey",
-// 		// background: "grey",
-// 	});
-// }
-
-// let objects = {};
-// let form = document.getElementById("form");
-// form.onsubmit = function (e) {
-// 	let name = document.getElementById("name").value;
-// 	let email = document.getElementById("email").value;
-// 	let partySize = document.getElementById("partySize").value;
-// 	let date = document.getElementById("date").value;
-// 	let time = document.getElementById("time").value;
-// 	let location = document.getElementById("location").value;
-// 	let contactNumber = document.getElementById("contactNumber").value;
-// 	let text = document.getElementById("my-textarea").value;
-
-// 	let result = Math.random().toString().slice(2, 11);
-
-// 	objects = {
-// 		result: {
-// 			name: name,
-// 			email: email,
-// 			partySize: partySize,
-// 			date: date,
-// 			time: time,
-// 			location: location,
-// 			contactNumber: contactNumber,
-// 			text: text,
-// 			reservation: result,
-// 		},
-// 	};
-// 	console.log(JSON.stringify(objects));
-// 	e.preventDefault();
-// };
 
 // party size feild
 $(function () {
@@ -153,32 +81,40 @@ $(function () {
 	}
 });
 
+// save reservation to localstorage
+
+function saveLocalTodos(glass) {
+	localStorage.setItem("reservation", JSON.stringify(glass.name));
+}
+
 // ******************************* checking reservation *******************************
 
 const todoList = document.querySelector(".todo-list");
 
 let refValue = document.querySelector(".resCheck-input");
 let modal = document.querySelector(".resCheck-button");
-modal.addEventListener("click", addTodo);
+modal.addEventListener("click", checkingRes);
 
-function addTodo(event) {
+function checkingRes(event) {
+	let localStore;
 	// prevent form from submitting
 	event.preventDefault();
 	// Todo Div
-	const todoDiv = document.createElement("div");
+	let todoDiv = document.createElement("div");
 	todoDiv.classList.add("todo");
 	// create Li
-	const newTodo = document.createElement("li");
-	newTodo.innerText = refValue.value;
+	let newTodo = document.createElement("li");
+	localStore = JSON.parse(localStorage.getItem("reservation"));
+	newTodo.innerText = localStore;
 	todoDiv.appendChild(newTodo);
-	console.log(newTodo);
+	console.log(localStore);
 	// check mark button
-	const completedButton = document.createElement("button");
+	let completedButton = document.createElement("button");
 	completedButton.innerHTML = '<i class="fas fa-check"></i>';
 	completedButton.classList.add("complete-btn");
 	todoDiv.appendChild(completedButton);
 	// check trash button
-	const trashButton = document.createElement("button");
+	let trashButton = document.createElement("button");
 	trashButton.innerHTML = '<i class="fas fa-trash"></i>';
 	trashButton.classList.add("trash-btn");
 	todoDiv.appendChild(trashButton);
@@ -192,31 +128,6 @@ function addTodo(event) {
 	let completebtn = document.querySelector(".complete-btn");
 	completebtn.addEventListener("click", popUp);
 }
-
-// reservation change popup
-
-// function popUp() {
-// 	let name = `${refValue.value}`;
-// 	let party = 5;
-// 	let date = "23/07/2021";
-// 	let reservation = Math.floor(Math.random() * (1000 - 100) + 100);
-// 	Swal.fire({
-// 		icon: "success",
-// 		title: `Reservation Number: ${reservation}`,
-// 		html: `Booking Name: ${name} <br> Group Size: ${party} <br>
-//   Booking Date: ${date} <br> <input type="text" value="haribo" name="" id="inputText" placeholder="teser">`,
-// 		showCancelButton: true,
-// 		confirmButtonText: "Close",
-// 		cancelButtonText: `<a
-// 		style="color: white;"
-// 		href="#collapseOne"
-// 		>Make a Change</a
-// 	>`,
-// 		cancelButtonColor: "red",
-// 		// backdrop: "lightgrey",
-// 		// background: "grey",
-// 	});
-// }
 
 function popUp() {
 	let name = `${refValue.value}`;
@@ -262,7 +173,7 @@ function popUp() {
 										<input
 											type="text"
 											name="name"
-											id="name"
+											id="names"
 											placeholder="Name"
 											class="form-control"
 										/>
@@ -272,7 +183,7 @@ function popUp() {
 											type="email"
 											class="form-control"
 											name="email"
-											id="email"
+											id="emails"
 											aria-describedby="emailHelpId"
 											placeholder="example@hotmail.co.uk"
 										/>
@@ -281,14 +192,14 @@ function popUp() {
 										<select
 											name=""
 											class="form-control"
-											id="partySize"
+											id="partySizes"
 										></select>
 
 										<label for="date">Date</label>
 										<input
 											type="date"
 											name="date"
-											id="date"
+											id="dates"
 											class="form-control"
 										/>
 
@@ -296,7 +207,7 @@ function popUp() {
 										<input
 											type="time"
 											name="time"
-											id="time"
+											id="times"
 											class="form-control"
 										/>
 
@@ -304,7 +215,7 @@ function popUp() {
 										<select
 											name="location"
 											class="location form-control"
-											id="location"
+											id="locations"
 										>
 											<option value="">See Below</option>
 											<option value="london">London</option>
@@ -318,19 +229,32 @@ function popUp() {
 										<input
 											type="tel"
 											name="contactNumber"
-											id="contactNumber"
+											id="contactNumbers"
 											placeholder="020 5701 7892"
 											class="form-control"
 										/>
 
 										<label for="my-textarea">Text</label>
 										<textarea
-											id="my-textarea"
+											id="my-textareas"
 											class="form-control"
-											name="textarea"
+							alert("yes");				name="textarea"
 											rows="3"
 											class="form-control"
 										></textarea>`,
+			}).then((result) => {
+				let glasses = {
+					name: document.getElementById("names").value,
+					email: document.getElementById("emails").value,
+					party: document.getElementById("partySizes").value,
+					date: document.getElementById("dates").value,
+					time: document.getElementById("times").value,
+					location: document.getElementById("locations").value,
+					// let contactNumber = document.getElementById("contactNumber").value;
+					text: document.getElementById("my-textareas").value,
+					reservation: Math.floor(Math.random() * 1000000000),
+				};
+				localStorage.setItem("Newreservation", JSON.stringify(glasses));
 			});
 		}
 	});
